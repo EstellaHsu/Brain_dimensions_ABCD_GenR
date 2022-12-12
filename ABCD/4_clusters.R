37ec488720f1e7adda24f1299893bc884aabb030
Bing Xu <73080574+EstellaHsu@users.noreply.github.com>
2022-12-12 16:28:04 +0100

    Update 4_clusters.R

--- ABCD/4_clusters.R
+++ ABCD/4_clusters.R
@@ -10,14 +10,14 @@
 ####### reorder the canonical variates
 rs_train_test_abcd <- readRDS("rs_train_test_abcd.rds")
 # the reference split: choose a split as a reference split
-load_std <- res.abcd.example$v # the loadings for CBCL
+load_ref <- res.abcd.example$v # the loadings for CBCL
 
 new_cv_reorder  <- lapply(seq_along(rs_train_test_abcd), function(j) {
   # go through all the splits
   idx_v <- c()
   for (i in 1:6) {
-    # calculate the maximum correlation of each CV and reorder
-    cor_cv <- sapply(1:6, function(z) {cor(load_std[, i], rs_train_test_abcd[[j]]$abcd.train$v[, z])})
+    # calculate the maximum correlation of each CV and reorder, we only care about the first six correlations (or any number)
+    cor_cv <- sapply(1:6, function(z) {cor(load_ref[, i], rs_train_test_abcd[[j]]$abcd.train$v[, z])})
     if(max(abs(cor_cv)) < 0.5){ # sometimes the correlation is too low
       idx  <- i
     } else {
@@ -39,15 +39,16 @@ new_cv_reorder  <- lapply(seq_along(rs_train_test_abcd), function(j) {
 
 ##### average brain loadings across 10 splits
 
-b <- lapply(seq_along(new_cv_reorder), function(x) {
+brain <- lapply(seq_along(new_cv_reorder), function(x) {
   loadMat <- rs_train_test_abcd[[x]]$abcd.train$u[, new_cv_reorder[[x]]$idx_reorder]})
 
 brain_mean <- do.call(cbind, lapply(1:6, 
                                 function(i) {
-                                  rowMeans(abs(do.call(cbind, lapply(b, function(x) {x[, i]})
+                                  rowMeans(abs(do.call(cbind, lapply(brain, function(x) {x[, i]})
                                   )))}))
 ##### calculate the CV scores
 # brain_whole is the original brain connecitivity features of the whole ABCD sample
+# i.e., it is the PCs calculated by weighted PCA based on the whole ABCD sample
 brain_cvscores_average10 <- scale(brain_whole) %*% brain_mean
 
 
@@ -56,11 +57,13 @@ brain_cvscores_average10 <- scale(brain_whole) %*% brain_mean
 ######### clustering 
 #############################################
 
-scca_brain <- readRDS("brain_cvscores_average10.rds")
-cca_brain_1 <- scca_brain[, 1:3]
+# choose the first 3 brain CVs
+scca_brain <- brain_cvscores_average10[, 1:3]
 
-# 1. hiearachical clustering
-d <- dist(cca_brain_1, method ="euclidean")
+######### the following code are from Dinga et al., 2019
+
+# hiearachical clustering
+d <- dist(scca_brain, method ="euclidean")
 res.hc <- hclust(d, method = "ward.D")
 clusters <- cutree(res.hc, k = 2)
 
@@ -68,8 +71,7 @@ df.clu.figure <- data.frame(CV1 = unlist(cca_brain_1[, 1]),
                             CV2 = unlist(cca_brain_1[, 2]),
                             CV3 = unlist(cca_brain_1[, 3]))
 df.clu.figure$clusters <- clusters
-ggplot(df.clu.figure) + geom_point(aes(x = CV1, y = CV2), colour= clusters) +
-  theme_bw()
+
 
 # Dendrogram
 d <- as.dendrogram(res.hc)
@@ -78,12 +80,12 @@ plot(d, main = "Dendrogram for hierarchical clustering")
 
 
 # decide how many clusters
-cca_rs_data <- cca_brain_1
-hcfit_ch <- NbClust(cca_rs_data, method="ward.D",
+# variance ratio criterion
+hcfit_ch <- NbClust(scca_brain, method="ward.D",
                     min.nc = 1, max.nc = 6, index = "ch")
-hcfit_sl <- NbClust(cca_rs_data, method="ward.D",
+# sihoutte
+hcfit_sl <- NbClust(scca_brain, method="ward.D",
                     min.nc = 1, max.nc = 6, index = "silhouette")
-
 
 fit_ch <- data.frame(number = 1:6, values = hcfit_ch$All.index)
 fit_sl <- data.frame(number = 1:6, values = hcfit_sl$All.index)
@@ -102,62 +104,37 @@ sl <- ggplot(fit_sl, aes(number, values))+
 
 grid.arrange(ch, sl, ncol=2)
 
-# 2. Fuzzy C means clustre analysislibrary(cluster)
+# 3-D visualization 
+df.clu.figure <- data.frame(CV1 = unlist(cca_brain_1[, 1]), 
+                            CV2 = unlist(cca_brain_1[, 2]),
+                            CV3 = unlist(cca_brain_1[, 3]))
+df.clu.figure$clusters <- clusters
 
-cluster_fuzzy_train <- cmeans(cca_brain_1 , centers=2, iter.max = 100, verbose = FALSE,
-                              dist = "euclidean", method = "cmeans", m = 2,
-                              rate.par = NULL, weights = 1, control = list())
-cluster.df <- data.frame(Hclu=clusters, Fclu_1=cluster_fuzzy_train$membership[,1], 
-                         Fclu_2 = cluster_fuzzy_train$membership[,2])
-dim(cluster.df)
-
-
-clu_stable <- cluster.df %>% filter(Fclu_1 > 0.7 | Fclu_2 > 0.7)
-dim(clu_stable)
-
-idx_clu <- which(cluster.df$Fclu_1 > 0.7 | cluster.df$Fclu_2 > 0.7)
-
-RS_stable <- data.frame(CV1 = unlist(cca_brain_1[-idx,1]), CV2 = unlist(cca_brain_1[-idx,2]),
-                        CV3 = unlist(cca_brain_1[-idx,3]))
-RS_stable$clusters <- as.factor(clusters[-idx])
-
-idx <- which(cca_brain_1[,2]== min(cca_brain_1[,2]))
-
-
-
-plot_ly(RS_stable, x = ~CV1, y = ~CV2, z = ~CV3, 
+plot_ly(df.clu.figure, x = ~CV1, y = ~CV2, z = ~CV3, 
         color = ~clusters, marker = list(size=3.5), colors = c("skyblue2", "gold1"))
 
-dim(cca_rs_data)
+
 # significance test
-sigma <- cov(cca_rs_data)
-mu <- colMeans(cca_rs_data)
-real_CI <- cluster_test(cca_rs_data)
+sigma <- cov(scca_brain)
+mu <- colMeans(scca_brain)
+real_CI <- cluster_test(scca_brain)
 
 cl <- makePSOCKcluster(8)
 registerDoParallel(cl)
-n_sims <- 99
+n_sims <- 999
 null <- foreach(i = 1:n_sims, .packages=c("NbClust","MASS")) %dopar% {
+      cluster_test <- function(cca_data){
+      hcfit <- NbClust(cca_data, method="ward.D", index="ch", min.nc=1, max.nc = 6)
+      CH_index <- max(hcfit$All.index, na.rm = TRUE)
+      hcfit <- NbClust(cca_data, method="ward.D", index="silhouette", min.nc=1, max.nc = 6)
+      sil_index <- max(hcfit$All.index, na.rm = TRUE)
+      return(c("CH"=CH_index, "Silhouette"=sil_index))
+     }
   rand_sample <- mvrnorm(n=nrow(cca_rs_data), mu=mu, Sigma=sigma)
   res <- cluster_test(rand_sample)
 }
 
 stopCluster(cl)
-
-
-
-cluster_test <- function(cca_data){
-  #ugly hack, because i don't know how to prevent this library creating many plots
-  hcfit <- NbClust(cca_data, method="ward.D", index="ch", min.nc=1, max.nc = 6)
-  CH_index <- max(hcfit$All.index, na.rm = TRUE)
-  hcfit <- NbClust(cca_data, method="ward.D", index="silhouette", min.nc=1, max.nc = 6)
-  sil_index <- max(hcfit$All.index, na.rm = TRUE)
-  return(c("CH"=CH_index, "Silhouette"=sil_index))
-}
-
-
-
-
 
 
 null_CI <- as.data.frame(do.call(rbind, null))
@@ -168,7 +145,7 @@ rank_cv2 <- sum(real_CI[2] < null_CI[,2]) + 1
 pval_cv2 <- rank_cv2 / (n_sims+1)
 t(t((c("p.val variance ratio"=pval_cv1, "p.val Silhouette"=pval_cv2))))
 
-
+# visualization of the significance test
 df.ch <- data.frame(cor = null_CI[,1])
 df.sl <- data.frame(cor = null_CI[,2])
 
@@ -188,25 +165,5 @@ sl2 <- ggplot(df.sl, aes(x=cor)) +  # Apply nrow function
   theme_bw()
 
 grid.arrange(ch1, sl2, ncol =2)
-
-
-
-
-###############################################
-######### clustering: demogrpahic distributions 
-###############################################
-setwd("~/Desktop/ABCD_download/data/newdata_ReleaseQC")
-
-all_demo <- readRDS("all_final_noNA_incidental_6529.rds")
-all_demo$clusters <- clusters
-
-demo_cbcl <- all_demo[,c("sex","race_ethnicity","parental_education",
-                        "cbcl_scr_syn_internal_r","cbcl_scr_syn_external_r",
-                        "cbcl_scr_syn_totprob_r","clusters")]
-summary(demo_cbcl[demo_cbcl$clusters == 1,])
-lapply(demo_cbcl[demo_cbcl$clusters == 1,4:6], sd)
-
-
-
 
 
