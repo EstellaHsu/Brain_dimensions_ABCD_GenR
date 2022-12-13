00177bc5e390fb90715137a9e8717f1ff0ca7c4e
Bing Xu <73080574+EstellaHsu@users.noreply.github.com>
2022-12-13 15:31:07 +0100

    Update 1_residulization_pca.R

--- GenerationR/1_residulization_pca.R
+++ GenerationR/1_residulization_pca.R
@@ -3,55 +3,57 @@
 ###############################################################################
 
 
+library(doParallel)
+library(permute)
 
 #####################################
-######## 1.residualization ##########
+######## 1.read the brain data ######
 #####################################
-
-########### data extraction
-    brain_train <- make_brain_features(subid_train)
-    brain_test <- make_brain_features(subid_test)
-    
-    cbcl_train <- train0[, c("cbcl_scr_syn_anxdep_r","cbcl_scr_syn_withdep_r","cbcl_scr_syn_somatic_r",
-                           "cbcl_scr_syn_social_r", "cbcl_scr_syn_thought_r","cbcl_scr_syn_attention_r",
-                           "cbcl_scr_syn_rulebreak_r","cbcl_scr_syn_aggressive_r")]
-    cbcl_test <- test0[,c("cbcl_scr_syn_anxdep_r","cbcl_scr_syn_withdep_r","cbcl_scr_syn_somatic_r",
-                           "cbcl_scr_syn_social_r", "cbcl_scr_syn_thought_r","cbcl_scr_syn_attention_r",
-                           "cbcl_scr_syn_rulebreak_r","cbcl_scr_syn_aggressive_r")]
-                           
-    confounders_train <- train0[,c("age","sex","race_ethnicity","site","parental_education")]
-    confounders_test <- test0[,c("age","sex","race_ethnicity","site","parental_education")]
-        
-    ############ residualization
-    brain_train_residual <- residualization(brain_train, confounders_train)
-    brain_test_residual <- residualization(brain_test, confounders_test)
-    
-    ############ weighted PCA
-    pca.weighted.rotation <- weighted_pca(cbcl_train, brain_train_residual, 100)
+cbcl_demo <- readRDS("final_imp_cbcl_covariates.rds")
+subid <- cbcl_demo$IDC
+conMatDir <- ('/home/r051950/resting_state_calculation/Gordon_new_AROMAOnly/corMatZ.hcpMMP_fsSC.8pAroma/')
+ 
+cl <- makePSOCKcluster(12)
+registerDoParallel(cl)
+ 
+feature_list <- foreach::foreach(i = 1:length(subid), .packages=c("doParallel", "foreach")) %dopar% {
+    subID <- paste('sub', subid[i], sep='-')
+    conMatCsv <- file.path(conMatDir, paste0(subID, '_corMatZ.Gordon_fsSC.8pAroma.txt'))
+    conMat <- as.matrix(read.table(conMatCsv))
+    feature_column <- conMat[upper.tri(conMat, diag = FALSE)]
+}
+ 
+stopCluster(cl)
+ 
+feature_genr <- do.call(rbind,feature_list)
+feature_genr <- as.data.frame(feature_genr)
+colnames(feature_genr) <- paste0("F_", 1:ncol(feature_genr))
 
 
-
-
-
-
-
-
-
-
-
-
-brain <- readRDS("feature_genr_aromaonly_notwinsib.rds")
-brain <- brain[, -ncol(brain)]
-confounders_imp <- readRDS("confounders_imp_cbcl_meduRelevel_aromaonly_notwinsib.rds")
+#####################################
+######## 2.residualization ##########
+#####################################
 
 # regress the feature matrix on age, gender, ethnicity and SES (maternal education)
 resi_brain <- lapply(1:(ncol(brain)), function(i) {
-   out <- residuals(lm(brain[, i] ~ confounders_imp$age + confounders_imp$gender + 
-                      factor(confounders_imp$ethnicity) + factor(confounders_imp$maternal_edu), na.action=na.exclude))
+   out <- residuals(lm(feature_genr[, i] ~ cbcl_demo$age + cbcl_demo$gender + 
+                      cbcl_demo$ethnicity + cbcl_demo$maternal_edu, na.action=na.exclude))
 })
-
 
 brain_residual <- do.call(cbind, resi_brain)
 colnames(brain_residual) <- paste0("F_", 1:ncol(brain_residual))
 
-saveRDS(brain_residual, "feature_residual_aromaonly_cbclitem.rds")
+#####################################
+######## 2.residualization ##########
+#####################################
+cbcl_genr <- cbcl_demo[, c("sum_anx_9m","sum_wit_9m","sum_som_9m","sum_sop_9m",
+                           "sum_tho_9m","sum_att_9m","sum_rul_9m","sum_agg_9m")]
+pca.weighted.genr <- weighted_pca(cbcl_genr, brain_residual, 100)
+# extract the first 100 brain PCs
+brain_genr <- pca.weighted.genr$brain_train_reduced
+
+
+
+
+
+
